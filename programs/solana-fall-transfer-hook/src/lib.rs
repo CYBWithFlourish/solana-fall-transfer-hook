5bae09e310a661749b140f2bf05331ee2fd9d560
FLOURISH <project.samclak@gmail.com>
2026-09-15 20:42:47 +0100

    build: register token-mover program and sync program ids

--- programs/solana-fall-transfer-hook/src/lib.rs
+++ programs/solana-fall-transfer-hook/src/lib.rs
@@ -11,7 +11,7 @@ pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
-declare_id!("2JfEsBs1CWdqsrgrPzqZywenRgjTP1cRwuybYyRtccuM");
+declare_id!("6TfxUdqLLLUTbV5HHcQmVnziTs8oBbLzWpBCZxEXhNoD");
 
 #[program]
 pub mod solana_fall_transfer_hook {
